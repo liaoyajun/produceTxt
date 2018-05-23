e3b456aed533f6a982e9bc6a5c9296dd7ef94d22
liaoyajun <coderliao@163.com>
2018-05-23 14:51:53 +0800

    存档

--- index05.js
+++ index05.js
@@ -3,7 +3,13 @@ var shell = require("shelljs");
 console.log('\033[2J');
 var sourceArr = ['~', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '=', '_', '+', '[', ']', '{', '}', '|', ';', ':', ',', '.', '/', '<', '>', '?', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'z', 'x', 'c', 'v', 'b', 'n', 'm', 'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', 'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', 'Z', 'X', 'C', 'V', 'B', 'N', 'M'];
 var maxvalue = sourceArr.length - 1;
-
+var time = new Date();
+function formDate(ms) {
+    var s = parseInt(ms / 1000);
+    var min = Math.floor(s / 60);
+    s = s - 60 * min;
+    return '\n 用时' + min + '分' + s + '秒';
+}
 function main(aa, bb, cc, dd, ee) {
     var strArr = []
     var count = 0
@@ -31,8 +37,11 @@ function main(aa, bb, cc, dd, ee) {
                                                 b = 0;
                                                 a = a + 1;
                                                 if (a > maxvalue) {
+                                                    console.log(formDate(new Date() - time));
                                                     console.log('\033[32m \n5位全部执行完毕 \033[0m');
-                                                    // shell.exec("node /Users/liaoyajun/lab/其他/produceTxt/index06.js");
+                                                    setTimeout(function() {
+                                                        // shell.exec("node /Users/liaoyajun/lab/其他/produceTxt/index06.js");
+                                                    }, 2000);
                                                     return;
                                                 }
                                             }
@@ -47,8 +56,12 @@ function main(aa, bb, cc, dd, ee) {
                         if (a == maxvalue && b == maxvalue && c == maxvalue && d == maxvalue && e == maxvalue) {
                             var str = strArr.join('\n') + '\n'
                             fs.appendFile('/Users/liaoyajun/lab/其他/produceTxt/password/password05.txt', str, function(err) {
+                                console.log(formDate(new Date() - time));
                                 console.log('\033[32m \n5位全部执行完毕 \033[0m');
-                                // shell.exec("node /Users/liaoyajun/lab/其他/produceTxt/index06.js");
+                                setTimeout(function() {
+                                    // shell.exec("node /Users/liaoyajun/lab/其他/produceTxt/index06.js");
+                                }, 2000);
+                                return;
                             });
                             return
                         }
